258ac1da442cbaca4f3c73288c7c2c716d669dd2
lidiasanchez <blue98.bee.dev@gmail.com>
2025-03-22 14:13:51 +0100

    Add documentation for current gdt topics

--- src/data/extra/extra-topics.js
+++ src/data/extra/extra-topics.js
@@ -1,3 +1,26 @@
 import BASE_TOPIC_GAMES from '../index.js';
+import {SetInfluence} from "../utils.js";
 
-const EXTRA_TOPICS =
+
+
+const addExtraTopic = (extraTopic) => {
+    return {
+        id: extraTopic.id,
+        audienceWeightings: {
+            young: SetInfluence(extraTopic.audienceWeightings[0]),
+            mature: SetInfluence(extraTopic.audienceWeightings[1]),
+            adult: SetInfluence(extraTopic.audienceWeightings[2]),
+        },
+        name: extraTopic.name,
+        genreWeightings: {
+            action: SetInfluence(extraTopic.genreWeightings[0]),
+            adventure: SetInfluence(extraTopic.genreWeightings[1]),
+            rol: SetInfluence(extraTopic.genreWeightings[2]),
+            simulation: SetInfluence(extraTopic.genreWeightings[3]),
+            strategy: SetInfluence(extraTopic.genreWeightings[4]),
+            casual: SetInfluence(extraTopic.genreWeightings[5]),
+        }
+    }
+}
+
+const EXTRA_TOPICS = addExtraTopic()
--- src/data/gdt/gdt-topics.js
+++ src/data/gdt/gdt-topics.js
@@ -58,4 +58,66 @@ export const BASE_TOPIC_GAMES = Topics.topics.map( topic => {
  * - Military
  * - Medieval
  * - Space
+ * - Racing
+ * - Fantasy
+ * - Pirate
+ * - Sci-fi
+ * - Airplane
+ * - Dungeon
+ * - Mystery
+ * - Martial Arts
+ * - History
+ * - Horror
+ * - Business
+ * - Transport
+ * - Comedy
+ * - Ninja
+ * - Romance
+ * - Movies
+ * - Spy
+ * - Detective
+ * - Cyberpunk
+ * - UFO
+ * - Hospital
+ * - Evolution
+ * - Time travel
+ * - Life
+ * - Virtual Pet
+ * - Vocabulary
+ * - Hunting
+ * - Law
+ * - Game Dev
+ * - City
+ * - School
+ * - Fashion
+ * - Zombies
+ * - Hacking
+ * - Government
+ * - Prison
+ * - Surgery
+ * - Music
+ * - Rythm
+ * - Superheroes
+ * - Post Apocalyptic
+ * - Alternate History
+ * - Vampire
+ * - Werewolf
+ * - Aliens
+ * - Wild West
+ * - Dance
+ * - Cooking
+ * - Farming
+ * - Crime
+ * - Disasters
+ * - Assassin
+ * - Thief
+ * - Colonization
+ * - Construction
+ * - Mythology
+ * - Abstract
+ * - Mad Science
+ * - Extreme Sports
+ * - Dystopian
+ * - Expedition
+ * - Technology
  */
